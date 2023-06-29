4744e96160681d2e8dbd66af33341f23ac72f08b
BluBerryJay <arodrigues@student.neumont.edu>
2023-06-29 12:20:06 -0600

    updated core

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,10 +1,16 @@
 
 using namespace std;
 #include <iostream>
-
+#include "../Source/Engine/Core/random.h"
+#include "../Source/Engine/Core/fileIO.h"
 int main() {
-#ifdef _DEBUG
-	cout << "Debug\n";
-#endif
-	cout << "Hello World\n";
+	kiko::seedRandom((unsigned int)time(nullptr));
+	cout << kiko::getFilePath() << endl;
+	kiko::setFilePath("Assets");
+	cout << kiko::getFilePath() << endl;
+	size_t size;
+	kiko::getFileSize("everything.txt", size);
+	for (int i = 0; i < 10; i++) {
+		cout << kiko::random(10, 20) << endl;
+	}
 }
