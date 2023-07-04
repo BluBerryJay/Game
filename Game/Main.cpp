58fcb76206d7860980715a5eef09d8c383e0939f
BluBerryJay <arodrigues@student.neumont.edu>
2023-07-04 17:12:57 -0600

    common property sheet was added

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -3,14 +3,46 @@ using namespace std;
 #include <iostream>
 #include "../Source/Engine/Core/random.h"
 #include "../Source/Engine/Core/fileIO.h"
+#include "../Source/Engine/Core/Memory.h"
+#include "../Source/Engine/Core/Time.h"
+#include "../Source/Engine/Renderer/Renderer.h"
+
+
 int main() {
-	kiko::seedRandom((unsigned int)time(nullptr));
+	
+	kiko::g_memoryTracker.displayInfo();
+	int* ptr = new int;
+	kiko::g_memoryTracker.displayInfo();
+	delete ptr;
+	kiko::g_memoryTracker.displayInfo();
+
+	kiko::Time timer;
+	for (int i = 0; i < 100000000; i++) {
+
+	}
+	cout << timer.GetElapsedSeconds() << endl;
+
+
+	
+	/*auto start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < 100000; i++) {
+
+	}
+	auto end = std::chrono::high_resolution_clock::now();
+	cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << endl;*/
+
+
+
+
+
+
+	/*kiko::seedRandom((unsigned int)time(nullptr));
 	cout << kiko::getFilePath() << endl;
 	kiko::setFilePath("Assets");
 	cout << kiko::getFilePath() << endl;
-	size_t size;
+	size_t size = 0;
 	kiko::getFileSize("everything.txt", size);
 	for (int i = 0; i < 10; i++) {
 		cout << kiko::random(10, 20) << endl;
-	}
+	}*/
 }
--- Source/Engine/Core/Memory.cpp
+++ Source/Engine/Core/Memory.cpp
@@ -3,16 +3,37 @@
 
 using namespace std;
 
+
+kiko::MemoryTracker kiko::g_memoryTracker;
 void* operator new (size_t size)
 {
-	cout << "allocated: " << size << endl;
+	void* ptr = malloc(size);
+	kiko::g_memoryTracker.Add(ptr, size);
+	//cout << "allocated: " << size << endl;
 
-	return malloc(size);
+	return ptr;
 }
 
 void operator delete (void* address, size_t size)
 {
-	cout << "deallocated: " << size << endl;
-
+	//cout << "deallocated: " << size << endl;
+	kiko::g_memoryTracker.Remove(address, size);
 	free(address);
+}
+namespace kiko {
+	void MemoryTracker::Add(void* address, size_t size)
+	{
+		m_bytesAllocated += size;
+		m_numAllocations++;
+	}
+	void MemoryTracker::Remove(void* address, size_t size)
+	{
+		m_bytesAllocated -= size;
+		m_numAllocations--;
+	}
+	void MemoryTracker::displayInfo()
+	{
+		cout << "current bytes allocated: " << m_bytesAllocated << endl;
+		cout << "current number allocated: " << m_numAllocations << endl;
+	}
 }
